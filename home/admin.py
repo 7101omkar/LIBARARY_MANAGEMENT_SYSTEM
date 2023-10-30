7aee5ec6ad266601542885a0926a42d1af04d936
7101omkar <141727978+7101omkar@users.noreply.github.com>
2023-10-30 21:31:11 +0530

    Delete home directory

--- home/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book, Members, Transaction, LibraryBook
-
-# Register your models here.
-
-admin.site.register(Book)
-admin.site.register(Members)
-admin.site.register(Transaction)
-admin.site.register(LibraryBook)
